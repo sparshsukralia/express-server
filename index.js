890a5e915c8d635e03d022f9d9acc5534062845d
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-10-20 12:29:04 +0545

    express server 5000

---
+++ index.js
@@ -0,0 +1,21 @@
+// importing express from the modules
+const express = require("express");
+const path = require("path");
+const members = require("./Members");
+
+// initializing express app
+const app = express();
+
+// Serving a static page to the server
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+app.get("/api/members", (req, res) => res.json(members));
+
+// setting a static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
